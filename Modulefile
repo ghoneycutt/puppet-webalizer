21210cb76d6b10515ce5c71650af28966ae64545
Garrett Honeycutt <github@garretthoneycutt.com>
2010-07-01 14:10:52 -0700

    initial publication

---
+++ Modulefile
@@ -0,0 +1,2 @@
+name    'ghoneycutt-webalizer'
+version '1.0.0'
---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+# Class: webalizer
+#
+# This module manages webalizer
+# http://www.webalizer.org/
+#
+class webalizer {
+
+    package { "webalizer": }
+} # class webalizer
